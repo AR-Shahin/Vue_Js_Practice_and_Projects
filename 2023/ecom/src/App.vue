596e5ec232013803f7456371506e46eac868006a
AR-Shahin <mdshahinmije96@gmail.com>
2022-12-17 19:19:02 +0600

    Ddddddd

--- 2023/ecom/src/App.vue
+++ 2023/ecom/src/App.vue
@@ -4,18 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">H=ome</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  
 
   <RouterView />
 </template>
